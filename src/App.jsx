0f2eae802daf6d251909b79636e4ec9cb1b2c905
rog-dsgn <goldgameboi@gmail.com>
2026-04-26 00:09:30 -0300

    Ajuste de rotas e inserido nova pagina de admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthProvider";
+import UserAuth from "./pages/admin/UserAuth";
+
+// pages
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/admin" element={<UserAuth />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 };
