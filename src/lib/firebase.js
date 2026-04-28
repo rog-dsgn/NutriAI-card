595eeb87e6b6de894ba5294ab8996db118bfff1c
rog-dsgn <goldgameboi@gmail.com>
2026-04-28 18:56:22 -0300

    rs

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQd8AQixe1TU4KnMbrSrJ3ykO1kyta7Yg",
-  authDomain: "nutri-ai-f1691.firebaseapp.com",
-  databaseURL: "https://nutri-ai-f1691-default-rtdb.firebaseio.com",
-  projectId: "nutri-ai-f1691",
-  storageBucket: "nutri-ai-f1691.firebasestorage.app",
-  messagingSenderId: "1054351251897",
-  appId: "1:1054351251897:web:9f9d4edd8aecdcf4e97e68",
+  apiKey: import.meta.env.VITE_FB_APIKEY,
+  authDomain: import.meta.env.VITE_FB_AUTH,
+  databaseURL: import.meta.env.VITE_FB_DATABASE,
+  projectId: import.meta.env.VITE_FB_PROJECT,
+  storageBucket: import.meta.env.VITE_FB_STORAGE,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGE_SENDER,
+  appId: import.meta.env.VITE_FB_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
